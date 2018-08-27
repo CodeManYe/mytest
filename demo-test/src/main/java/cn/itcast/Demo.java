7a0dcb9668f56a89fa19e9f55c2991d941a43031
zero <2010029594@qq.com>
2018-08-27 19:52:24 +0800

    第三次修改

--- demo-test/src/main/java/cn/itcast/Demo.java
+++ demo-test/src/main/java/cn/itcast/Demo.java
@@ -10,6 +10,5 @@ package cn.itcast;
 public class Demo {
     public static void main(String[] args){
        System.out.println("=====1111======");
-	   System.out.println("=====22222======");
     }
 }
